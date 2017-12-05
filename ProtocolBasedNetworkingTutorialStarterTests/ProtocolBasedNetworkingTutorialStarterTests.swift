7f640dcf9dd292f0d4731420986f8211667b33a7
james <jamesrochabrun@gmail.com>
2017-12-04 21:20:20 -0800

    Initial Commit

---
+++ ProtocolBasedNetworkingTutorialStarterTests/ProtocolBasedNetworkingTutorialStarterTests.swift
@@ -0,0 +1,36 @@
+//
+//  ProtocolBasedNetworkingTutorialStarterTests.swift
+//  ProtocolBasedNetworkingTutorialStarterTests
+//
+//  Created by James Rochabrun on 12/4/17.
+//  Copyright © 2017 James Rochabrun. All rights reserved.
+//
+
+import XCTest
+@testable import ProtocolBasedNetworkingTutorialStarter
+
+class ProtocolBasedNetworkingTutorialStarterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
