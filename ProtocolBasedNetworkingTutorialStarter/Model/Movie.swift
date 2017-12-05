b105fa71823813f6ecb7116d131a8261c625fa3e
james <jamesrochabrun@gmail.com>
2017-12-04 21:27:30 -0800

    first commit

---
+++ ProtocolBasedNetworkingTutorialStarter/Model/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  ProtocolBasedNetworkingTutorialStarter
+//
+//  Created by James Rochabrun on 12/4/17.
+//  Copyright © 2017 James Rochabrun. All rights reserved.
+//
+
+import Foundation
