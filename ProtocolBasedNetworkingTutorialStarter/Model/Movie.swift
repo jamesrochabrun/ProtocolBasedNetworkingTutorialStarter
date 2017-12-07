141b9d9610156418733d45e71e0ad2a93e6fe5b2
James Rochabrun <james@deemelo.com>
2017-12-06 16:58:32 -0800

    Update Movie.swift

--- ProtocolBasedNetworkingTutorialStarter/Model/Movie.swift
+++ ProtocolBasedNetworkingTutorialStarter/Model/Movie.swift
@@ -14,8 +14,9 @@ import Foundation
 struct Movie: Decodable {
     
     let title: String?
-    let posterPath: String?
+    let poster_path: String?
     let overview: String?
     let releaseDate: String?
-    let coverPath: String?
+    let backdrop_path: String?
+    let release_date: String?
 }
